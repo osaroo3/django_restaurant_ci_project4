d8eecc4cb6ff823dc254ef9cce1373c09acd760f
Arukwe Obo-Eba <oboebafriday@gmail.com>
2024-03-17 08:03:38 +0000

    deployment checks

--- africana/settings.py
+++ africana/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7b#momfzw07!bte*)&safm6#im9d&%sr$*wh9q3agf=tg^do-i'
+# SECRET_KEY = 'django-insecure-7b#momfzw07!bte*)&safm6#im9d&%sr$*wh9q3agf=tg^do-i'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
